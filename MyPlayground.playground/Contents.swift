34a31a20da12119e16f11e125beed0553e2d6ad2
Michal <m.lukac@icloud.com>
2022-12-26 17:49:12 +0100

    01_verz (proměnné, konstanty, camelCase, PascalCase, comment)

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,5 +1,36 @@
 import UIKit
 
 var greeting = "Hello, playground"
-var pozdrav = "Dobré ráno"
+var pozdrav1 = "Dobré ráno"
+
+
+//Premenné
+//deklarácia
+var meno = "Jano"
+meno = "Ivan"
+meno = "Pavol"
+
+var moja_premenná = true
+
+
+// Konštanta
+let postava = "Adam Durica"
+print(postava)
+
+//CamelCase
+let pracovnaPozice = "Manažer"
+print(pracovnaPozice)
+
+//PascalCase
+struct AkoChces{
+    
+}
+
+//komentáre
+// jednoriadkové
+/*
+ viacriadkové
+ */
+
+var obrazok = "nieco.jpg" // komentujeme aj za
 
