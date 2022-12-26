3e287addac71f9ec2c1b173794c925b4585ea3d1
Michal <m.lukac@icloud.com>
2022-12-26 23:13:31 +0100

    1.úkol_hotovo

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -52,3 +52,23 @@ print(vetaObjemKocky)
  4. Chopok je na slovensku najveternejšie miesto. Priemerná ročná rýchlosť vetra je 10 m/s.
  Aká je priemerná rýchlosť vetra v jednotkách km/h a ktas?Programovanie je kažodenné riešenie problémov. Koľko ktas je 1km/h? Hľadajte, googlite, študujte :)
  */
+
+let priemernaRocnaRychlostVetraMetre: Double = 10
+let vypocetMetrNaKm: Double = priemernaRocnaRychlostVetraMetre * 3.5999971
+var promenaA : Double = vypocetMetrNaKm
+var vypocetKts : Double = promenaA * 0.53996
+let veta04 = "Priemerná ročná rýchlosť vetra na Chopku je \(vypocetMetrNaKm) km/h a ktas = \(vypocetKts) ktas."
+print(veta04)
+
+
+/*
+ 5. 29.11.1965 bola najvyššia nameraná rýchlosť vetra na slovensku na Skalnatom Plese. Vietor dosahoval rýchlosť 283 km/h.
+ Chcem vedieť aká rýchlosť to je v m/s a ktas.
+ */
+let najvyssiaNameranaRychlostKmH : Double = 283
+let vypocetKmNaMetr : Double = najvyssiaNameranaRychlostKmH / 3.5999971
+promenaA = najvyssiaNameranaRychlostKmH
+var vypocetKts2 : Double = promenaA * 0.53996
+let veta05 = "Najvyššia rýchlosť je \(vypocetKmNaMetr) m/s a ktas bolo \(vypocetKts2) ktas."
+print(veta05)
+
