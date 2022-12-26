2abc7d5cc7dcb8c653d0a22a6610574048a4a815
Michal <m.lukac@icloud.com>
2022-12-26 18:14:04 +0100

    02.(String, Int, Double, Bool)

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -34,3 +34,54 @@ struct AkoChces{
 
 var obrazok = "nieco.jpg" // komentujeme aj za
 
+//02
+//úvodzovky
+let citacia = "Napíšem vetu a ked chcem použit úvodzovky tak musím pridať obrátené lomítko, asi takto \"STOJ\" a nebude hlásit chybu"
+
+// viacriadková syntax
+let recept = """
+500g múku
+20 vajec
+1 liter mlieka
+"""
+
+//práce s textom
+
+var silaVetra = "mierny vietor"
+print(silaVetra.count)
+print(silaVetra.uppercased())
+var prazdnePole = "ahoj"
+print(prazdnePole.isEmpty)
+
+// Integer (z latinského slova)
+
+let vstupne = 5
+
+var zarobok = vstupne * 12
+let zostatokNaUcte = 1_000_000_000_000_000_000
+ 
+// Double
+let pi = 3.14592654
+let teplota = -15.5
+
+// Bool
+let rybaPlavaVoVode = true
+
+//Prefix a Suffix
+let obrazok2 = "fotka.jpg"
+obrazok2.hasPrefix("fotka")
+obrazok2.hasSuffix("jpg")
+
+// delitelnost čísla
+let rodneCislo = 8504017321
+rodneCislo.isMultiple(of: 14)
+100.isMultiple(of: 2)
+
+
+
+
+
+
+
+
+
