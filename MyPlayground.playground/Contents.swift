5acb1f9e03b353d0c65363a8efd260f9a43ac53b
Michal <m.lukac@icloud.com>
2022-12-26 22:05:25 +0100

    Domácá úloha 1

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,133 +1,21 @@
 import UIKit
-
-var greeting = "Hello, playground"
-var pozdrav1 = "Dobré ráno"
-
-
-//Premenné
-//deklarácia
-var meno = "Jano"
-meno = "Ivan"
-meno = "Pavol"
-
-var moja_premenná = true
-
-
-// Konštanta
-let postava = "Adam Durica"
-print(postava)
-
-//CamelCase
-let pracovnaPozice = "Manažer"
-print(pracovnaPozice)
-
-//PascalCase
-struct AkoChces{
-    
-}
-
-//komentáre
-// jednoriadkové
 /*
- viacriadkové
- */
+1. Spojte nasledovné stringy dokopy
+auto / servis
+ľadový / čaj
+poly / styrén
 
-var obrazok = "nieco.jpg" // komentujeme aj za
+2. Automatizuj vzorec na výpočet obvodu obdĺžnika
 
-//02
-//úvodzovky
-let citacia = "Napíšem vetu a ked chcem použit úvodzovky tak musím pridať obrátené lomítko, asi takto \"STOJ\" a nebude hlásit chybu"
+3. Automatizuj vzorec na výpočet objemu kocky
 
-// viacriadková syntax
-let recept = """
-500g múku
-20 vajec
-1 liter mlieka
-"""
+4. Chopok je na slovensku najveternejšie miesto. Priemerná ročná rýchlosť vetra je 10 m/s.
+Aká je priemerná rýchlosť vetra v jednotkách km/h a ktas?Programovanie je kažodenné riešenie problémov. Koľko ktas je 1km/h? Hľadajte, googlite, študujte :)
 
-//práce s textom
+5. 29.11.1965 bola najvyššia nameraná rýchlosť vetra na slovensku na Skalnatom Plese. Vietor dosahoval rýchlosť 283 km/h.
+Chcem vedieť aká rýchlosť to je v m/s a ktas.
+*/
 
-var silaVetra = "mierny vietor"
-print(silaVetra.count)
-print(silaVetra.uppercased())
-var prazdnePole = "ahoj"
-print(prazdnePole.isEmpty)
-
-// Integer (z latinského slova)
-
-let vstupne = 5
-
-var zarobok = vstupne * 12
-let zostatokNaUcte = 1_000_000_000_000_000_000
- 
-// Double
-let pi = 3.14592654
-let teplota = -15.5
-
-// Bool
-let rybaPlavaVoVode = true
-
-//Prefix a Suffix
-let obrazok2 = "fotka.jpg"
-obrazok2.hasPrefix("fotka")
-obrazok2.hasSuffix("jpg")
-
-// delitelnost čísla
-let rodneCislo = 8504017321
-rodneCislo.isMultiple(of: 14)
-100.isMultiple(of: 2)
-
-//03.diel
-5 + 1
-(4 - 2)*5
-3*3
-//modulo
-
-var modulo1 = 6%2
-print(modulo1)
-var modulo2 = 9%4
-print(modulo2)
-modulo2 = 9 % 5
-print(modulo2)
-
-//04 Združené operátory
-
-var odbehnuteKilometre = 6
-var dnesSomOdbehol = 3
-
-var odbehnuteKilometreCelkovo = odbehnuteKilometre + dnesSomOdbehol
-var odbehnuteKilometreCelkovo2 = odbehnuteKilometre
-odbehnuteKilometreCelkovo2 += dnesSomOdbehol
-/*
-    +=
-    -=
-    *=
-    %=
- */
-
-// 05. Type Annotation
-
-let meno05 : String = "Alice"
-var skore: Double = 0
-
-//pretipovanie
-
-let prveCislo = 1
-let druheCislo = 2.4
-
-let vysledok05 = Double(prveCislo) + druheCislo
-
-let pocetPsov05 = "Doma máme psov :"
-let pocetPsov05a = 2
-
-let celkovyPocetPsov = pocetPsov05 + String(pocetPsov05a)
-
-//06. String Interpolation
-let vek06 = 28
-let vypis06 = "mám \(vek06) rokov"
-let vsetko06 = "Dnes \(vypis06) a je to fajn"
-
-let cislo06 = 8
-let spocitanie06 = "\(cislo06) + \(cislo06) = \(cislo06 + cislo06)"
-print(spocitanie06)
-
+//1.
+let automobil = "auto"
+let servis1 = "servis"
